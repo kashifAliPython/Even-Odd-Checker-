a2cc4369aef29eda8d706a4674b3cae858812c26
kashif Ali <kashifalisultan4455@gmail.com>
2026-02-01 00:05:55 +0500

    Add files via upload

---
+++ Even-odd-checker.py
@@ -0,0 +1,7 @@
+             # Check the number is even or odd
+
+num = int(input("Enter the Number : "))
+if num %2==0: 
+    print("The Number you have Entered is Even",num)
+else:
+    print("The Number You have Entered is Odd",num)
